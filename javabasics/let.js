19cd9783fcc84c010ce24eaa5cb521b4e8f5b658
MA <maria.argandona@icloud.com>
2026-01-16 10:56:14 -0400

    Criar let.js

---
+++ javabasics/let.js
@@ -0,0 +1,16 @@
+// ===============================
+// let can change its value
+// ===============================
+
+// A let variable can change its value after being declared.
+
+let macaco = "Bugio";
+
+// Reassigning a new value to the same variable
+macaco = "Sagui";
+
+// console.log prints the value of the variable
+console.log(macaco);
+
+// Output in the console:
+// Sagui
