b2a6e093b1c66ced5441232577b3f893f1a89d37
MA <maria.argandona@icloud.com>
2026-01-16 08:58:39 -0400

    Criar console.js

---
+++ javabasics/console.js
@@ -0,0 +1,24 @@
+// ===============================
+// Console and execution order
+// ===============================
+
+// The computer follows instructions line by line.
+// This means the code is executed in sequence, from top to bottom.
+
+// To check if the code is working, we use the console.
+// The console shows the result (output) of an action.
+
+// To send information to the console, we use:
+console.log("example");
+
+// -------------------------------
+// Example
+// -------------------------------
+
+let macaco = "banana";
+
+// console.log sends the value of the variable to the console
+console.log(macaco);
+
+// Output in the console:
+// banana
