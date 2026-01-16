eba437f172dc826b0e936a1db94c2b8eb8895552
MA <maria.argandona@icloud.com>
2026-01-16 12:45:26 -0400

    Atualizar o addingVariables.js

--- javabasics/addingVariables.js
+++ javabasics/addingVariables.js
@@ -13,3 +13,24 @@ console.log(macaco + cor);
 
 // Output in the console:
 // Bugiovermelho
+
+// ===============================
+// Sum with const variables
+// ===============================
+
+// Constants are used when the value should not change.
+
+const IDADE_MULHER = 34;
+const IDADE_HOMEM = 40;
+
+// We can sum numeric constants using the + operator.
+// Because these values are numbers (not strings),
+// JavaScript performs a mathematical sum.
+
+const TOTAL = IDADE_MULHER + IDADE_HOMEM;
+
+// console.log displays the result in the console
+console.log(IDADE_MULHER + IDADE_HOMEM);
+
+// Output in the console:
+// 74
