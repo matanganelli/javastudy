cb1c5b6e55ca4aed3661c40454ed33310d74d75d
MA <maria.argandona@icloud.com>
2026-01-16 12:34:05 -0400

    Criar addingVariables.js

---
+++ javabasics/addingVariables.js
@@ -0,0 +1,15 @@
+// ===============================
+// Adding (concatenating) variables
+// ===============================
+
+// Variables can be added together.
+// When we use + with strings, JavaScript concatenates the values.
+
+const macaco = "Bugio";
+const cor = "vermelho";
+
+// Concatenation using +
+console.log(macaco + cor);
+
+// Output in the console:
+// Bugiovermelho
