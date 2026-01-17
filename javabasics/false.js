42c2cf70d54aa4f0d58f5bc0020ca31a675e101c
MA <maria.argandona@icloud.com>
2026-01-16 22:55:34 -0400

    Criar false.js

---
+++ javabasics/false.js
@@ -0,0 +1,9 @@
+// false é o OPOSTO de true
+// Também é um valor BOOLEANO
+// NÃO usa aspas
+
+// false indica que algo está DESATIVADO
+
+const macaco = false;
+
+// Agora o recurso "macaco" está desligado
