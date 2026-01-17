f23ffe6d60f68364953553f679817b66125804ec
MA <maria.argandona@icloud.com>
2026-01-17 12:01:13 -0400

    Criar operadorNegação.js

---
+++ javabasics/operadorNegação.js
@@ -0,0 +1,16 @@
+// Operador de negação (!)
+// O operador ! inverte o valor booleano
+
+// true se torna false
+const macaco = !true;
+console.log(macaco); // false
+
+// false se torna true
+const banana = !false;
+console.log(banana); // true
+
+// O operador ! sempre retorna um valor booleano
+const ligado = true;
+const desligado = !ligado;
+
+console.log(desligado); // false
