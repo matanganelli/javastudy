bc035f45ddb49af6f209a23f88a3905169d957c9
MA <maria.argandona@icloud.com>
2026-01-16 22:55:04 -0400

    Criar true.js

---
+++ javabasics/true.js
@@ -0,0 +1,13 @@
+// true é um VALOR, não uma string
+// Por isso, NÃO usa aspas ("true" estaria errado)
+
+// true é um valor BOOLEANO
+// Boolean só pode ser: true ou false
+
+// É muito usado para indicar que algo está ATIVADO
+// Ex: um recurso ligado, uma permissão ativa, uma condição satisfeita
+
+const macaco = true;
+
+// Aqui, a variável macaco guarda o valor booleano true
+// Isso significa que o "recurso macaco" está habilitado
